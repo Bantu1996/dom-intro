85847da11e75521847285c4d2495847a9e10df68
Bantu1996 <sotyatobantu@gmail.com>
2020-05-05 13:04:31 +0200

    more Updates made.

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -6,7 +6,6 @@ const addBillTypeButton = document.querySelector(".addBillTypeBtn");
 const updateSettingsBtn = document.querySelector(".updateSettings");
 
 const callCostSettings = document.querySelector(".callCostSetting");
-
 const smsCostSettings = document.querySelector(".smsCostSetting");
 const warningLevelSettings = document.querySelector(".warningLevelSetting");
 const criticalLevelSettings = document.querySelector(".criticalLevelSetting");
@@ -18,63 +17,72 @@ const settingTotalBillSpanElement = document.querySelector(".blue")
 
 var settingCallTotal = 0;
 var settingSmsTotal = 0;
+var settingTotalBill = 0;
+var currentSettingTotalBill = 0;
 
 
 function billTypeTotal() {
     const checkedSettingsRadioButtons = document.querySelector(".billItemTypeWithSettings:checked");
     if (checkedSettingsRadioButtons){
      var billItemTypeWithSettings = checkedSettingsRadioButtons.value
-    
-if (billItemTypeWithSettings === "call") {
-    settingCallTotal += callCostSetting;
-}
 
- else if (billItemTypeWithSettings === "sms") {
-    settingSmsTotal += smsCostSetting;
+ if (settingTotalBill < criticalLevelSetting)  { 
+switch (billItemTypeWithSettings) {
+    case "call":
+    settingTotalBill += callCostSetting;
+    settingCallTotal += callCostSetting;
+    break;
+    case "sms":
+     settingTotalBill += smsCostSetting;
+    settingSmsTotal += smsCostSetting
+    break;
 }
+}
+selectingSettingBillColor();
 
     settingCallTotalElement.innerHTML = settingCallTotal.toFixed(2);
     settingSmsTotalElement.innerHTML = settingSmsTotal.toFixed(2);
-    var settingTotalBill = settingCallTotal + settingSmsTotal;
+    settingTotalBill = settingCallTotal + settingSmsTotal;
     settingTotalBillElement.innerHTML = settingTotalBill;
+
 }
-return settingTotalBill;
+// return settingTotalBill;
 }
-function selectingSettingBillColor(settingTotalBill) {
+
+
+function selectingSettingBillColor() {
 
 var currentSettingTotalBill = Number(settingTotalBill);
 
+ settingTotalBillSpanElement.classList.remove("warning");
+    settingTotalBillSpanElement.classList.remove("danger");
 
-settingTotalBillSpanElement.classList.remove("danger");
-settingTotalBillSpanElement.classList.remove("warning");
-
-warningLevelSetting = warningLevelSettings.value;
-criticalLevelSetting = criticalLevelSettings.value;
-if (currentSettingTotalBill >= criticalLevelSetting){
-
-    settingTotalBillSpanElement.classList.add("danger");   
-}
-else if (currentSettingTotalBill >= warningLevelSetting && currentSettingTotalBill <= criticalLevelSetting){
-    
+if (currentSettingTotalBill >= warningLevelSetting && currentSettingTotalBill <= criticalLevelSetting){
+    settingTotalBillSpanElement.classList.remove("danger");
     settingTotalBillSpanElement.classList.add("warning");  
 }
+
+else if (currentSettingTotalBill >= criticalLevelSetting){
+    settingTotalBillSpanElement.classList.remove("warning");
+    settingTotalBillSpanElement.classList.add("danger");   
+}
+ 
+
 }
 
-function checkedBillTypeBtn() {
+var callCostSetting =  0;
+var smsCostSetting =  0;
+var warningLevelSetting = 0;
+var criticalLevelSetting = 0;
 
-    var billItemTypeWithSettings = settingsRadioButtons.value;
-    settingTotalBill = billTypeTotal();
-    selectingSettingBillColor(settingTotalBill);
-
-}
 function checkedSettingBillBtn() {
     
     callCostSetting =  Number(callCostSettings.value);
     smsCostSetting =  Number(smsCostSettings.value);
     warningLevelSetting = Number(warningLevelSettings.value);
     criticalLevelSetting = Number(criticalLevelSettings.value);
-
+selectingSettingBillColor();
 }
-addBillTypeButton.addEventListener("click", checkedBillTypeBtn)
+addBillTypeButton.addEventListener("click",billTypeTotal)
 
 updateSettingsBtn.addEventListener("click", checkedSettingBillBtn)
